11631022ebc7e8f81eb645c14da71b2e12073367
Prakash Mishra <pm9507041@gmail.com>
2021-11-06 22:42:16 +0530

    Reversing Array

--- binarySearch.js
+++ binarySearch.js
@@ -34,6 +34,7 @@
 // // }
 
 let array = [5, 10, 15, 13, 20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
+var arr = [9,4,6,8,10,6,7,8,20,13,12,15,16,18,17,19,1,5,2,3]
 
 function binarySearch(arr, value){
     let = start = 0;
@@ -50,8 +51,15 @@ function binarySearch(arr, value){
             end = mid - 1
         }
     }
-    return -1;
+    return 'not found';
+
 }
 
-result = binarySearch(array, 105)
+result = binarySearch(array, 75)
+console.log(result)
+
+sorted = arr.sort(function(a,b){return a -b});
+//console.log(sorted);
+
+result = binarySearch(sorted, 18)
 console.log(result)
