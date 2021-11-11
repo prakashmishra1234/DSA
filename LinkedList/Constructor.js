73b4a79465ec427677a217d25cc200141bf8c8a2
Prakash Mishra <pm9507041@gmail.com>
2021-11-11 18:10:15 +0530

    Linkedlist-Constructor

---
+++ LinkedList/Constructor.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head
+        this.length = 1
+    }
+}
+
+let myLinkedList = new LinkedList(4)
+console.log(myLinkedList)
+
+/*
+In the above code line 17 calls the class linkedlist and the new keyword calls
+the constructor of the class Linkedlist
+And the Node in line 10 calls the class node and new keyword calls constructor
+the class node.
+
+In the above class head and tail point to the same thing
+*/
