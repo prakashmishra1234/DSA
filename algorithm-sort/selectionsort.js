3d5d323b51421ffa9b4e8b27036ff57133a2645a
DevPrakashHub <prakash@sflhub.com>
2022-05-30 12:16:23 +0530

    Selection sort added

---
+++ algorithm-sort/selectionsort.js
@@ -0,0 +1,17 @@
+function selectionsort(array) {
+  let min;
+  for (let i = 0; i < array.length; i++) {
+    min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) min = j;
+    }
+    if (i !== min) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  return array;
+}
+
+console.log(selectionsort([1, 2, 3, 9, 7, 5, 53, 4, 3]));
