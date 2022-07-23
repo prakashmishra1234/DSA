ae47d272cf36ba3c8f0931da4a3c54b5dbcc8317
DevPrakashHub <prakash@sflhub.com>
2022-07-23 20:30:31 +0530

    bfs and dfs

--- BigO/Logitems1.js
+++ BigO/Logitems1.js
@@ -1,11 +1,11 @@
 // O(N) -- Number of operations is proportional to N
 
-function logItems(n){
-    for(let i=0; i<n; i++){
-        console.log(i);
-    };
-};
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+}
 
-logItems(10)
+logItems(10);
 
-//In the above example number of iteration is equal to n which is 10;ff
+//In the above example number of iteration is equal to n which is 10;
